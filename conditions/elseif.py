baed5cc7d9ef451e87da110cd7ba195ea4517568
Rajesh Rathod <96156957+rajeshrathod972@users.noreply.github.com>
2023-03-06 20:18:33 +0530

    python loops ifelse and elif

---
+++ conditions/elseif.py
@@ -0,0 +1,10 @@
+a=int(input("enter the input"))
+b=int(input("enter the input"))
+c=int(input("enter the input"))
+
+if(a>b):
+    print("a is max")
+elif(b>c):
+    print("b is max")
+else:
+    print("c is max")        
---
+++ conditions/ifelse.py
@@ -0,0 +1,9 @@
+a=int(input("enter the input"))
+b=int(input("enter the input"))
+
+
+if(a>b):
+    print("a is max")
+else:
+    print("b is max")    
+
